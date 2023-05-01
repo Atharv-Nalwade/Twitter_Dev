28c3dbfa9dd088fec0ba7bb932f8d629e1a940bc
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-05-01 15:19:21 +0530

    feat: Added CrudRepository and extended it in LikeRepository

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,51 +1,37 @@
-import Tweet from '../models/tweet.js';
+import Tweet from "../models/tweet.js";
+import CrudRepository from "./crud-repository.js";
 
-class TweetRepository{
+class TweetRepository extends CrudRepository {
+  constructor() {
+    super(Tweet);
+  }
 
-    async create(data){
-       try {
-          const tweet = await Tweet.create(data);
-          return tweet;
-       } catch (error) {
-          console.log(error);
-       }
+  async create(data) {
+    try {
+      const tweet = await Tweet.create(data);
+      return tweet;
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async get(id){
-        try {
-            const tweet = await Tweet.findById(id);
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
+  async getWithComments(id) {
+    try {
+      const tweet = await Tweet.findById(id).populate("comments").lean();
+      return tweet;
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async getWithComments(id){
-        try {
-            const tweet = await Tweet.findById(id).populate('comments').lean();
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
+  async getAll(limit, offset) {
+    try {
+      const tweet = await Tweet.find().skip(offset).limit(limit);
+      return tweet;
+    } catch (error) {
+      console.log(error);
     }
-
-    async getAll(limit,offset){
-        try {
-            const tweet = await Tweet.find().skip(offset).limit(limit);
-            return tweet; 
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async destroy(id){
-        try {
-            const tweet = await Tweet.findByIdAndRemove(id);
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
-    }
+  }
 }
 
-export default  TweetRepository;
+export default TweetRepository;
