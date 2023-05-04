4a2f225d0c628ca34077d86871bba2422dc1d125
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-05-04 11:39:30 +0530

    feat: Added passport logic and functionality

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from 'bcrypt';
+import  jwt  from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -24,6 +25,17 @@ userSchema.pre('save', function (next) {
     user.password = encryptedPassword;
     next();
 });
+
+// One way to define methods that can be used on instances of a Mongoose schema is by using the .methods() function.
+userSchema.methods.comparePassword = function compare(password) {
+    return bcrypt.compareSync(password, this.password);
+}
+
+userSchema.methods.genJWT = function generate() {
+    return jwt.sign({id: this._id, email: this.email}, 'twitter_secret', {
+        expiresIn: '1h'
+    });
+}
 
 const User = mongoose.model('User', userSchema);
 
