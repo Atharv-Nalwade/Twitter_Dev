420e3287ec7a0f9250d5f4c85a731bbd10c4d174
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-05-05 12:34:06 +0530

    test: Tested execute and helper from dummy-service.js

--- src/services/dummy-service.js
+++ src/services/dummy-service.js
@@ -1,23 +1,26 @@
-
-export const helper = () =>{
-  const num = Math.floor(Math.random()*10);
-  return num%2 == 0;
-}
+import { helper } from "./helper-service.js";
 
 export const execute = () =>{
-    // const result = helper();   Commenting it and hardcoding avlues
-    if(true){
+    const result = helper();
+    if(result){
         return 'Learning Js'
     }else{
         return 'Learning ReactJs'
     }
 }
 
+// Testing ways
+// export const helper = () =>{
+//   const num = Math.floor(Math.random()*10);
+//   return num%2 == 0;
+// }
+
 // export const execute = () =>{
-//     const result = helper();
-//     if(result){
+//     // const result = helper();   Commenting it and hardcoding avlues
+//     if(true){
 //         return 'Learning Js'
 //     }else{
 //         return 'Learning ReactJs'
 //     }
 // }
+
--- tests/services/dummy.test.js
+++ tests/services/dummy.test.js
@@ -1,7 +1,22 @@
-const { execute } = require("../../src/services/dummy-service.js");
+import {execute} from "../../src/services/dummy-service.js";
+import {helper} from "../../src/services/helper-service.js";
+
+jest.mock("../../src/services/helper-service.js")
 
 test('Result is true and Learning JS', () => {
     //Implementation goes here
+    // const spy = jest.spyOn(dummyFunctions,'helper').mockReturnValue(true);
+    // dummyFunctions.helper.mockImplementation(() => true);
+    helper.mockReturnValue(true);
+    const result =execute();  
+    expect(result).toBe('Learning Js')
+})
+
+test('Result is false and Learning ReactJs', () => {
+    //Implementation goes here
+    // const spy = jest.spyOn(dummyFunctions,'helper').mockReturnValue(true);
+    // dummyFunctions.helper.mockImplementation(() => false);
+    helper.mockReturnValue(false);
     const result = execute();
-    expect(result).toBe('Learning ')
+    expect(result).toBe('Learning ReactJs');
 })
